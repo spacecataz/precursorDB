fb9ec2dada8b7a6e16865c93e89571eacc71be7f
Dominic Kotzer <dominickotzer@gmail.com>
2020-09-20 18:43:20 -0500

    Standardized docstrings and formatting. Rebuilt test_midpoint() for midpointYZ(). Moved original contents of main to its' own method.

--- src/csheet_integrator.py
+++ src/csheet_integrator.py
@@ -1,19 +1,20 @@
-'''
-A module for calculating the proposed ground perturbation due
-to a interplanetary current sheet before it comes in contact with the 
-Earth's upstream bow shock.
-'''
+"""
+Module that calculates the ground perturbation from a current sheet.
+
+Calculates the proposed ground perturbation due to an interplanetary current
+sheet before it comes in contact with the Earth's upstream bow shock.
+"""
 
 import numpy as np
 
-### Some useful constants:
+# Some useful constants:
 mu0 = 4*np.pi*1E-7
-Re  = 6371000 # Earth radius in meters.
+Re = 6371000  # Earth radius in meters.
+
 
 def midpointYZ(f, y_start, y_end, y_n, z_start, z_end, z_n):
     """
-    Calculates an approximation of the integral for 'f(a) da' using the
-        Midpoint Rule. Here our values of da are on the y-z plane.
+    Approximate the integral for 'f(y, z) dy dz' using the Midpoint Rule.
 
     Parameters
     ----------
@@ -37,22 +38,22 @@ def midpointYZ(f, y_start, y_end, y_n, z_start, z_end, z_n):
     Returns
     -------
     result : float
-        DESCRIPTION.
+        The approximated result of the integral for 'f(a) da'.
 
     """
     # Function for the DeltaX components of the midpoint rule
-    delta = lambda a, b, n: (b - a) / float(n)
-    
+    def delta(a, b, n): return (b - a) / float(n)
+
     # Get the DeltaY and DeltaZ values
     del_y = delta(y_start, y_end, y_n)
     del_z = delta(z_start, z_end, z_n)
-    
+
     result = 0
     # Iterate over the range of all intervals in the y-axis
     for dy in range(y_n):
         # Calculate the position of the y-midpoint for this interval
         y_position = y_start + (del_y/float(2)) + (del_y * dy)
-        # Iterate over the range of all intervals in the z-axis for each y-axis interval
+        # Iterate over intervals in the z-axis for each y-axis interval
         for dz in range(z_n):
             # Calculate the position of the z-midpoint for this interval
             z_position = z_start + (del_z/float(2)) + (del_z * dz)
@@ -61,133 +62,164 @@ def midpointYZ(f, y_start, y_end, y_n, z_start, z_end, z_n):
     return result
 
 
-def midpoint(f, a, b, c, d, nx, ny):
-    '''
-    Description of function and all inputs. <3
-    '''
-    
-    hx = (b - a)/float(nx)
-    hy = (d - c)/float(ny)
-    I = 0
-    for i in range(nx):
-        for j in range(ny):
-            xi = a + hx/2 + i*hx
-            yj = c + hy/2 + j*hy
-            I += hx*hy*f(xi, yj)
-    return I
+def test_midpointYZ(tol=1E-5):
+    """
+    Test the midpointYZ() function using ideal.
 
-def test_midpoint(tol = 1E-5):
-    '''
-    Test our midpoint problem using ideal
-    '''
+    Parameters
+    ----------
+    tol : float, optional
+        Tolerance of error in the result. The default is 1E-5.
 
-    f = lambda x, y: 2*x + y
-    result = midpoint(f, 0, 2, 2, 3, 5, 5)
+    Raises
+    ------
+    ValueError
+        Raise when result is outside of tolerance.
 
-    expected = 9.0 # This should be what we get!
+    Returns
+    -------
+    None.
 
-    # If result is different than expected answer more than
-    # a set tolerance, throw a fit.
+    """
+    def f(y, z): return 2*y + z
+    result = midpointYZ(f, 0, 2, 5, 2, 3, 5)
+    expected = 9.0  # This should be what we get
+    success = True
+
     if np.abs(result-expected) > tol:
-        print('Result = {}, Expected = {}'.format(result, expected))
-        raise ValueError('Did not return correct result!!!')
+        success = False
+
+    print('Test midpointYZ:\n  Result   | {}\n  Expected | {}\n  Success  | {}'
+          .format(result, expected, success))
+
 
 def gen_kernel(R, ky, kz):
-    '''
-    Create functions that represent the kernel for the integral, both for
-    by and bz.
+    """
+    Create kernel functions for the biot-savart integrals.
 
-    ###i'm tooo lazy to change this now.
-    The function that is the integral kernel for the biot-savart function.
-    "R" is the distance of the center of the sheet from Earth (r-x) and
-    y, z are the coordinates along the sheet where we are currently 
-    integrating.
-    Kx, Ky are the strength of the current sheet.
-    Returns: Bz portion of integral kernel.
-    UNITS: Use meters and amp/meters.  SI, fool.
-    '''
-    
-# These need to be checked over.
-    by = lambda y,z: 0 
-    bz = lambda y,z: 0 #(kx*y-ky*R)/np.sqrt( np.sqrt(y**2+z**2) + R**2 )
-    
-    return by, bz
-    
-def biot_savart(R, ky, kz, width=128, dX=0.1):
-    '''
-    Using the midpoint integrator, calculate the surface magnetic field
-    perturbation from a uniform interplanetary current sheet.
-
-    Parameters:
-    ==========
+    Parameters
+    ----------
     R : float
-        Distance from Earth in meters
-    ky : float 
-        Current sheet strength in the GSM Y-direction (amps/m)
-    kz : float 
-        Current sheet strength in the GSM Z-direction (amps/m)
-    
+        Distance in meters from the Earth to the center of current sheet (r-x).
+    ky : float
+        Current sheet strength in the GSM Y-direction (amps/m).
+    kz : float
+        Current sheet strength in the GSM Z-direction (amps/m).
 
-    Other parameters:
-    =================
-    width : float
-        Width of current sheet in both Y and Z directions in Re.
-        Defaults to 128 earth radii.
-    dX    : float
-        Size of spatial step for integration in Earth radii.
-        Defaults to 0.1Re.
+    Returns
+    -------
+    y_kernel : lambda
+        Integral kernel function for the GSM Y-direction.
+    z_kernel : lambda
+        Integral kernel function for the GSM Z-direction.
 
-    Returns:
-    ===========
-    by, bz : float, float
-        Perturbation as measured by ground at center of Earth in the
-        Y, Z directions (GSM).
+    Notes
+    -----
+    y : float
+        GSM Y-coortinate along the current sheet where we are integrating.
+    z : float
+        GSM Z-coordinate along the current sheet where we are integrating.
 
-    '''
+    """
+    '''These need to be checked over.'''
+    def y_kernel(y, z): return 0
+    # Old Equation for z: (kx*y-ky*R)/np.sqrt( np.sqrt(y**2+z**2) + R**2 )
+    def z_kernel(y, z): return 0  # Temporarily zeroed.
 
+    return y_kernel, z_kernel
+
+
+def biot_savart(R, ky, kz, width=128, dX=0.1):
+    """
+    Calculate surface magnetic field perturbation from a uniform current sheet.
+
+    Parameters
+    ----------
+    R : float
+        Distance from the Earth to the center of current sheet (r-x).
+    ky : float
+        Current sheet strength in the GSM Y-direction (amps/m).
+    kz : float
+        Current sheet strength in the GSM Z-direction (amps/m).
+    width : float, optional
+        Width of current sheet in Y and Z directions (Re). The default is 128.
+    dX : float, optional
+        Size of spatial step for integration (Re). The default is 0.1.
+
+    Returns
+    -------
+    by : float
+        Perturbation measured by ground at center of Earth in GSM Y-direction.
+    bz : float
+        Perturbation measured by ground at center of Earth in GSM Z-direction.
+
+    """
     # Get number of steps required to finish integral:
     nx = int(width/dX)
 
     # Create integral kernels:
     kern_y, kern_z = gen_kernel(R, ky, kz)
-    
-    # Integrate!
-    # by = 0 # Hey fix this in the future.
-    bz = midpoint(kern_z, width/2, width/2, width/2, width/2, nx, nx)
-    
-    return(bz)
-    
-def calc_K(b1, b2):
-    '''
-    For *b1* (downstream magnetic field, or field closer to the Earth)
-    and *b2* (upstream magnetic field, or further from the Earth), calculate
-    the current sheet required to sustain the change between the fields.
-    Return current density in amperes/meter.
 
-    Magnetic field should be given in nanotesla!
-    '''
-    
-# Does this match our calculations?
+    # Integrate to solve for by, bz.
+    by = 0  # Hey fix this in the future.
+    bz = midpointYZ(kern_z, width/2, width/2, width/2, width/2, nx, nx)
+
+    return(by, bz)
+
+
+def calc_K(b1, b2):
+    """
+    Calculate K of the current sheet from magnetic fields on either side.
+
+    Parameters
+    ----------
+    b1 : float
+        Downstream magnetic field in nanotesla (closer to the Earth).
+    b2 : float
+        Upstream magnetic field in nanotesla (further from the Earth).
+
+    Returns
+    -------
+    float
+        Current density K in amperes/meter.
+
+    """
+    # Does this match our calculations?
     return 1E-9*(b1-b2)/mu0
-    
-if __name__ == '__main__':
-    # This is the default action if you run this code!
+
+
+def main_old():
+    """
+    Old contents of main.
+
+    Returns
+    -------
+    None.
+
+    """
     print('Testing against Extreme SSI simulation!')
 
     # Get our current sheet strength using values from simulation:
     Ky = calc_K(-5, 127)
     Kz = 0
-    
+
     # Set distance from Earth:
-    R = 20 * Re # 20 RE from Earth! 
-    
+    R = 20 * Re  # 20 RE from Earth
+
     # Integrate!
-    bz = biot_savart(R, Ky, Kz)
-    
-# I'm not sure what this does.
+    by, bz = biot_savart(R, Ky, Kz)
+    print(bz)
+
+    ''' I'm not sure what this does.'''
     # To get time series, loop over an array of R-values:
     x_all = np.arange(150, 15, -1)
     b_all = np.zeros(x_all.size)
 
     for i, x in enumerate(x_all):
-        b_all[i] = biot_savart(x, Ky, Kz)
+        trash, b_all[i] = biot_savart(x, Ky, Kz)
+
+
+if __name__ == '__main__':
+    # This is the default action if you run this code
+    test_midpointYZ()
+    # main_old()
