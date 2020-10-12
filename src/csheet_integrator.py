6ec6fe742461ae5fe021c42e35ea012f415c6ac7
Dominic Kotzer <dominickotzer@gmail.com>
2020-10-12 15:12:13 -0500

    Changed bz equation in biot_savart(), created test method for biot_savart (needs valid inputs), modified main_old() into gmp_timeseries().

--- src/csheet_integrator.py
+++ src/csheet_integrator.py
@@ -71,26 +71,23 @@ def test_midpointYZ(tol=1E-5):
     tol : float, optional
         Tolerance of error in the result. The default is 1E-5.
 
-    Raises
-    ------
-    ValueError
-        Raise when result is outside of tolerance.
-
     Returns
     -------
     None.
 
     """
-    def f(y, z): return 2*y + z
-    result = midpointYZ(f, 0, 2, 5, 2, 3, 5)
     expected = 9.0  # This should be what we get
     success = True
 
-    if np.abs(result-expected) > tol:
+    def f(y, z): return 2*y + z
+    result = midpointYZ(f, 0, 2, 5, 2, 3, 5)
+    diff = np.abs(result - expected)
+
+    if diff > tol:
         success = False
 
-    print('Test midpointYZ:\n  Result   | {}\n  Expected | {}\n  Success  | {}'
-          .format(result, expected, success))
+    print('Test {}:\n  Result   | {}\n  Expected | {}\n  Success  | {}'
+          .format('midpointYZ', result, expected, success))
 
 
 def gen_kernel(R, ky, kz):
@@ -121,10 +118,11 @@ def gen_kernel(R, ky, kz):
         GSM Z-coordinate along the current sheet where we are integrating.
 
     """
+    # print("{}, {}, {}".format(R, ky, kz))
     '''These need to be checked over.'''
     def y_kernel(y, z): return 0
     # Old Equation for z: (kx*y-ky*R)/np.sqrt( np.sqrt(y**2+z**2) + R**2 )
-    def z_kernel(y, z): return 0  # Temporarily zeroed.
+    def z_kernel(y, z): return (kz * R - ky * R) / (R**2 + y**2 + z**2)
 
     return y_kernel, z_kernel
 
@@ -162,9 +160,36 @@ def biot_savart(R, ky, kz, width=128, dX=0.1):
 
     # Integrate to solve for by, bz.
     by = 0  # Hey fix this in the future.
-    bz = midpointYZ(kern_z, width/2, width/2, width/2, width/2, nx, nx)
+    bz = midpointYZ(kern_z, -width/2, width/2, nx, -width/2, width/2, nx)
 
     return(by, bz)
+
+
+def test_biot_savart(tol=1E-5):
+    """
+    Test the biot_savart() function using a known solution.
+
+    Parameters
+    ----------
+    tol : float, optional
+        Tolerance of error in the result. The default is 1E-5.
+
+    Returns
+    -------
+    None.
+
+    """
+    expected = (0, 0)
+    success = True
+
+    result = biot_savart(0, 0, 0)
+    diff = tuple(map(lambda a, b: np.abs(a - b), result, expected))
+
+    if diff[0] > tol or diff[1] > tol:
+        success = False
+
+    print('Test {}:\n  Result   | {}\n  Expected | {}\n  Success  | {}'
+          .format('biot_savart', result, expected, success))
 
 
 def calc_K(b1, b2):
@@ -188,38 +213,48 @@ def calc_K(b1, b2):
     return 1E-9*(b1-b2)/mu0
 
 
-def main_old():
+def gmp_timeseries(IMFd, IMFu, Vsw, dT=10):
     """
-    Old contents of main.
+    Solve for time series of ground magnetic perturbation due to current sheet.
+
+    Parameters
+    ----------
+    IMFd : float
+        Downstream IMF (Earth side of current sheet).
+    IMFu : float
+        Upstream IMF (Sun side of current sheet).
+    Vsw : float
+        Solar wind velocity.
+    dT : int, optional
+        Time between integrations. The default is 10.
 
     Returns
     -------
     None.
 
     """
-    print('Testing against Extreme SSI simulation!')
-
     # Get our current sheet strength using values from simulation:
-    Ky = calc_K(-5, 127)
+    Ky = calc_K(IMFd, IMFu)  # (-5, 127)
     Kz = 0
+    print(Ky)
 
     # Set distance from Earth:
     R = 20 * Re  # 20 RE from Earth
 
     # Integrate!
     by, bz = biot_savart(R, Ky, Kz)
-    print(bz)
+    print(by, bz)
 
-    ''' I'm not sure what this does.'''
     # To get time series, loop over an array of R-values:
-    x_all = np.arange(150, 15, -1)
-    b_all = np.zeros(x_all.size)
+    # x_all = np.arange(150, 15, -1)
+    # b_all = np.zeros(x_all.size)
 
-    for i, x in enumerate(x_all):
-        trash, b_all[i] = biot_savart(x, Ky, Kz)
+    # for i, x in enumerate(x_all):
+    #     trash, b_all[i] = biot_savart(x, Ky, Kz)
 
 
 if __name__ == '__main__':
     # This is the default action if you run this code
-    test_midpointYZ()
-    # main_old()
+    # test_midpointYZ()
+    test_biot_savart()
+    # gmp_timeseries(-5, 127, 0)
