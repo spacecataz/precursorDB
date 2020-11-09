5efa38cc93efd04ff2bbd00137e5efc39da7cc51
DSKotzer <dominickotzer@gmail.com>
2020-11-08 22:16:58 -0600

    Fixed the numbers going down instead of up by removing R where it should have been a unit vector component in the kernel.

--- src/csheet_integrator.py
+++ src/csheet_integrator.py
@@ -8,7 +8,7 @@ sheet before it comes in contact with the Earth's upstream bow shock.
 import numpy as np
 
 # Some useful constants:
-mu0 = 4*np.pi*1E-7
+mu0 = 4 * np.pi * 1E-7
 Re = 6371000  # Earth radius in meters.
 
 
@@ -42,7 +42,8 @@ def midpointYZ(f, y_start, y_end, y_n, z_start, z_end, z_n):
 
     """
     # Function for the DeltaX components of the midpoint rule
-    def delta(a, b, n): return (b - a) / n
+    def delta(a, b, n):
+        return (b - a) / n
 
     # Get the DeltaY and DeltaZ values
     del_y = delta(y_start, y_end, y_n)
@@ -52,11 +53,11 @@ def midpointYZ(f, y_start, y_end, y_n, z_start, z_end, z_n):
     # Iterate over the range of all intervals in the y-axis
     for i in range(y_n):
         # Calculate the position of the y-midpoint for this interval
-        y_position = y_start + (del_y/2) + (del_y * i)
+        y_position = y_start + (del_y / 2) + (del_y * i)
         # Iterate over intervals in the z-axis for each y-axis interval
         for j in range(z_n):
             # Calculate the position of the z-midpoint for this interval
-            z_position = z_start + (del_z/2) + (del_z * j)
+            z_position = z_start + (del_z / 2) + (del_z * j)
             # Add the calculated value for the summation at this y-z interval
             result += (del_y * del_z) * f(y_position, z_position)
     return result
@@ -79,7 +80,9 @@ def test_midpointYZ(tol=1E-5):
     expected = 9.0  # This should be what we get
     success = True
 
-    def f(y, z): return 2*y + z
+    def f(y, z):
+        return 2 * y + z
+
     result = midpointYZ(f, 0, 2, 5, 2, 3, 5)
     diff = np.abs(result - expected)
 
@@ -118,11 +121,13 @@ def gen_kernel(R, ky, kz):
         GSM Z-coordinate along the current sheet where we are integrating.
 
     """
-    # print("{}, {}, {}".format(R, ky, kz))
-    '''These need to be checked over.'''
-    def y_kernel(y, z): return 0
-    # Old Equation for z: (kx*y-ky*R)/np.sqrt( np.sqrt(y**2+z**2) + R**2 )
-    def z_kernel(y, z): return (kz * R - ky * R) / (R**2 + y**2 + z**2)
+    rhat_x = 1  # should this actually be a normalized value?
+
+    def y_kernel(y, z):
+        return kz * rhat_x / (R**2 + y**2 + z**2)
+
+    def z_kernel(y, z):
+        return -ky * rhat_x / (R**2 + y**2 + z**2)
 
     return y_kernel, z_kernel
 
@@ -152,26 +157,23 @@ def biot_savart(R, ky, kz, width=128, dX=0.1):
         Perturbation measured by ground at center of Earth in GSM Z-direction.
 
     """
+    halfw = width * Re / 2  # Units: m
 
-    # Change units of width and dX from RE to meters:
-    dX    *= Re
-    width *= Re
-    
-    # Get number of steps required to finish integral:
-    nx = int(width/dX)
+    # Get number of steps required to finish integral
+    nx = int(width / dX)
 
-    # Create integral kernels:
+    # Create integral kernels
     kern_y, kern_z = gen_kernel(R, ky, kz)
 
-    # Integrate to solve for by, bz.
-    by = 0  # Hey fix this in the future.
-    bz = midpointYZ(kern_z, -width/2, width/2, nx, -width/2, width/2, nx)
+    # Integrate to solve for by, bz.  Units: Amps
+    by = 0
+    bz = midpointYZ(kern_z, -halfw, halfw, nx, -halfw, halfw, nx)
 
-    # Convert units to Tesla from Amps:
-    by *= mu0/(4*np.pi)
-    bz *= mu0/(4*np.pi)
-    
-    return(by, bz)
+    # Convert to nanotesla from Amps.  4piE-7/4pi = 1E-7 => 1E-7 * 1E9 = 1E2
+    by *= 1E2
+    bz *= 1E2
+
+    return(by, bz)  # Units: nT
 
 
 def test_biot_savart(tol=1E-5):
@@ -217,14 +219,14 @@ def calc_K(b1, b2):
     float
         Current density K in amperes/meter.
 
-    """
-    # Does this match our calculations?
-    #return 1E-9*(np.abs(b1)+np.abs(b2))/mu0
+    Notes
+    -----
+    The calculation here takes into account the direction such that dL around
+        our amperian loop is in the positive Z direction on the b1 side.
 
-    # The calculation here takes into account the direction such that
-    # dL around our amperian loop is in the positive Z direction on the
-    # b1 side.
-    return 1E-9 * (b1 - b2) /mu0 #Units: A/m
+    """
+    return 1E-9 * (b1 - b2) / mu0  # Units: A/m
+
 
 def gmp_timeseries(IMFd, IMFu, Vsw, dT=10):
     """
@@ -243,7 +245,8 @@ def gmp_timeseries(IMFd, IMFu, Vsw, dT=10):
 
     Returns
     -------
-    None.
+    numpy.array
+        Timeseries of t_elapsed, By, and Bz.  Units: s, nT, nT
 
     """
     # Get our current sheet strength using values from simulation:
@@ -252,40 +255,23 @@ def gmp_timeseries(IMFd, IMFu, Vsw, dT=10):
     print(f"Ky = {Ky:0.4f}")
 
     # Set distance from Earth:
-    R = 32 * Re  # 20 RE from Earth
-
-    # Integrate!
-    # by, bz = biot_savart(R, Ky, Kz)
-    # print(by, bz)
+    R = 32 * Re  # 32 RE from Earth
 
     dX = Vsw * 1000 * dT
     x_position = R
     t_elapsed = 0
     timeseries = []
     while x_position > 10 * Re:
-        print(f"X: {x_position/Re:12.4f}RE | T: {t_elapsed:5.1f}s | ", end="")
+        line = f"X: {x_position/Re:12.4f}RE | T: {t_elapsed:5.1f}s | "
         by, bz = biot_savart(x_position, Ky, Kz)
-        # Convert to nanotesla:
-        by *= 1e9
-        bz *= 1e9
-        print(f"By: {by:0.2e}nT | Bz: {bz:0.2e}nT")
+        print(line + f"By: {by:0.2e}nT | Bz: {bz:0.2e}nT")
         timeseries.append((t_elapsed, by, bz))
-        t_elapsed  += dT
+        t_elapsed += dT
         x_position -= dX
 
-    # Return to caller
     return np.array(timeseries)
-
-    # To get time series, loop over an array of R-values:
-    # x_all = np.arange(150, 15, -1)
-    # b_all = np.zeros(x_all.size)
-
-    # for i, x in enumerate(x_all):
-    #     trash, b_all[i] = biot_savart(x, Ky, Kz)
 
 
 if __name__ == '__main__':
     # This is the default action if you run this code
-    # test_midpointYZ()
-    # test_biot_savart()
     gmp_timeseries(-5, 127, 2700)
